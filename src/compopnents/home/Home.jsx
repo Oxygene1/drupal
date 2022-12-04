c454087fea9e7aa7954a133f2e95df337a69c034
mac <mac@macs-MacBook-Pro.local>
2022-12-04 07:43:47 +0100

    music wiget added

--- src/compopnents/home/Home.jsx
+++ src/compopnents/home/Home.jsx
@@ -1,16 +1,51 @@
 import React from "react";
 import Header from "../header/Header";
-
-// import MuiHeader from "../muiHeader/MuiHeader";
+import { Typography } from "@mui/material";
+import Rating from "../mui/Ratings";
+import Card from "../mui/Card";
+import StreamIcon from "@mui/icons-material/Stream";
+import Music from "../mui/Music";
 const Home = () => {
   return (
-    <div className="home-container">
-      <div>
-        <Header />
-      </div>
+    <div>
+      <div className="home-container">
+        <div>
+          <Header />
+        </div>
+        <div style={{ padding: "4rem" }}></div>
+        <div className="first-home-container">
+          <div className="future">
+            <Typography>Welcome to the Future of music</Typography>
+          </div>
 
-      <h1>Hello Home</h1>
-      <div></div>
+          <div className="future">
+            <Typography>
+              We are rated exactly the way we deliver the sound
+            </Typography>
+            <Rating />
+          </div>
+
+          <div>
+            <StreamIcon
+              className="stream"
+              sx={{
+                color: "#706c6c",
+                width: "4rem",
+                height: "4rem",
+              }}
+            />
+          </div>
+          <div className="second-home-container">
+            <Music />
+          </div>
+        </div>
+      </div>
+      <div className="card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
